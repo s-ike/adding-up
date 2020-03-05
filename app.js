3250d03ede3033a4ad3f63a2511b3b1c5d046df0
s-ike <shunt_i@yahoo.co.jp>
2020-03-05 23:06:49 +0900

    ランキングの実装完了

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 'use strict';
+const fs = require('fs');
+const readline = require('readline');
+const rs = fs.createReadStream('./popu-pref.csv');
+const rl = readline.createInterface({ 'input': rs, 'output': {} });
+const prefDataMap = new Map();
+rl.on('line', (lineString) => {
+    const columns = lineString.split(',');
+    const year = parseInt(columns[0]);
+    const pref = columns[1];
+    const popu = parseInt(columns[3]);
+    if (year === 2010 || year === 2015) {
+        let value = prefDataMap.get(pref);
+        if (!value) {
+            value = {
+                popu10: 0,
+                popu15: 0,
+                change: null
+            };
+        }
+        if (year === 2010) {
+            value.popu10 = popu;
+        }
+        if (year === 2015) {
+            value.popu15 = popu;
+        }
+        prefDataMap.set(pref, value);
+    }
+});
+rl.on('close', () => {
+    for (let [key, value] of prefDataMap) {
+        value.change = value.popu15 / value.popu10;
+    }
+    const rankingArray = Array.from(prefDataMap).sort((pair1, pair2) => {
+        return pair2[1].change - pair1[1].change;
+    });
+    const rankingStrings = rankingArray.map(([key, value]) => {
+        return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率: ' + value.change; 
+    });
+    console.log(rankingStrings);
+})
